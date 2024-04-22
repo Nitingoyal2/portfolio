a4b49e06c289f2139b114e49cc13ea8f52159be7
nitin <nitingoyal658930@gmail.com>
2024-04-22 16:28:54 +0530

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -19,16 +19,35 @@ const About = () => {
                             <h1 className=''>
                                 About Me
                             </h1>
-                            <p>My name is Nitin Kumar, I am a self-motivated web developer with a completed Master degree in Computer Application from Punjabi University Patiala. I am proficient in a variety of programming languages and technologies, including HTML, CSS, JavaScript,Bootstrap,React JS.I completed full stack web development Internship. During this Internship, I successfully created a dynamic Investopedia website on my own. I learned so much about web development during this internship, and I completed courses and earned certificates from top companies like Internshala. I am also a quick learner and am always eager to take on new challenges.My goal for the future is to become a leading web developer. I want to work on challenging projects that will allow me to use my skills and knowledge to the fullest. I also want to help others learn about web development and share my passion for the field. I am excited to continue my journey as a web developer and to use my skills to make a positive impact on the world.Now, I am working in Sunfocus Solutions Pvt. Ltd as ReactJS traniee.During this trainee, I successfully completed project named as Music Mix Website.</p>
-                            <div className='social-icons'>
-                                <GitHubIcon className='icons' />
+                            <p>My name is Nitin Kumar, I am a self-motivated web developer with a completed Master degree in Computer Application from Punjabi University Patiala. I am proficient in a variety of programming languages and technologies, including HTML, CSS, JavaScript,Bootstrap,React JS.I completed full stack web development Internship. During this Internship, I successfully created a dynamic Shark Tank named website on my own. I learned so much about web development during this internship, and I completed courses and earned certificates from top companies like Internshala. I am also a quick learner and am always eager to take on new challenges.My goal for the future is to become a leading web developer. I want to work on challenging projects that will allow me to use my skills and knowledge to the fullest. I also want to help others learn about web development and share my passion for the field. I am excited to continue my journey as a web developer and to use my skills to make a positive impact on the world.Now, I am working in Sunfocus Solutions Pvt. Ltd as ReactJS traniee.During this trainee, I successfully completed project named as Music Mix Website.</p>
 
-                                <a href='https://www.instagram.com/_goyal_nitin._02/' target='_blank' rel='noopener noreferrer'>
-                                    <InstagramIcon className='icons' />
-                                </a>
-                                <a href='https://www.linkedin.com/in/nitin-kumar-a77b181b1' target='_blank' rel='noopener noreferrer'>
-                                    <LinkedInIcon className='icons' />
-                                </a>
+                            <div class="social-icons-wrapper">
+                                <div class="icon instagram">
+                                    <div class="tooltip">
+                                        Instagram
+                                    </div>
+                                    <a href='https://www.instagram.com/_goyal_nitin._02/' target='_blank' rel='noopener noreferrer'>
+                                        <span>
+                                            <InstagramIcon className='icons ' />
+                                        </span>
+                                    </a>
+                                </div>
+                                <div class="icon github">
+                                    <div class="tooltip">
+                                        Github
+                                    </div>
+                                    <span><a href='https://github.com/Nitingoyal2' target='_blank' rel='noopener noreferrer'>
+                                        <GitHubIcon className='icons ' />
+                                    </a></span>
+                                </div>
+                                <div class="icon youtube">
+                                    <div class="tooltip">
+                                        LinkedIn
+                                    </div>
+                                    <span> <a href='https://www.linkedin.com/in/nitin-kumar-a77b181b1' target='_blank' rel='noopener noreferrer'>
+                                        <LinkedInIcon className='icons' />
+                                    </a></span>
+                                </div>
                             </div>
                         </Col>
                     </Row>
